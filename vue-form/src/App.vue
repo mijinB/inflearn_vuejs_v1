9241ba378d5b703d000551168efc3b66a491ef62
mijinB <bmj13465@gmail.com>
2023-11-23 11:21:29 +0900

    input submit 기능구현

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id:</label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw:</label>
-      <input id="password" type="password" />
+      <input id="password" type="password" v-model="password" />
     </div>
-    <button>login</button>
+    <button type="submit" v-on:click="submitForm">login</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function (event) {
+      event.preventDefault();
+      console.log(this.username, this.password);
+    },
+  },
+};
 </script>
 
 <style>
